82cc7e4da6f83cf50a5ebade247e0bc0704ca04f
David Zheng <81645746+davidzhengyes@users.noreply.github.com>
2023-12-20 11:41:56 -0500

    works, times out
    times out on 62/71.

---
+++ pythonprac/two-pointers/container-most-water.py
@@ -0,0 +1,31 @@
+#https://leetcode.com/problems/container-with-most-water/
+#method ?
+#surely you cant go through each combination of two lines.
+#maybe find the two greatest at every interval and find the max of that?
+
+#time limit exceeded 52/61 testcases
+def maxArea(height):
+
+    gap=1
+
+    n=len(height)
+    totalmax=0
+    while gap<n:
+
+        lp=0
+        rp=lp+gap
+        currmax=0
+        while rp<n:
+            minheight=min(height[lp],height[rp])
+            vol=gap*minheight
+            if vol>currmax:
+                currmax=vol
+            lp+=1
+            rp+=1
+        gap+=1
+            
+        if currmax>totalmax:
+            totalmax=currmax
+        
+    return totalmax
+
