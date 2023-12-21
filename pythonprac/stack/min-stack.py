658885861d92c16182a9115dbfa3694f9ad6b139
David Zheng <81645746+davidzhengyes@users.noreply.github.com>
2023-12-20 19:06:41 -0500

    well didnt remember classes
    pretty self explanatory. did it after checking soln for basic concept.

---
+++ pythonprac/stack/min-stack.py
@@ -0,0 +1,21 @@
+class MinStack:
+    def __init__(self):
+        self.stack=[]
+        self.min=[]
+
+    def push(self,val):
+        self.stack.append(val)
+        if self.min:
+            self.min.append(min(val,self.min[-1]))
+        else:
+            self.min.append(val)
+
+    def pop(self):
+        self.stack.pop()
+        self.min.pop()
+
+    def top(self):
+        return self.stack[-1]
+
+    def getMin(self):
+        return self.min[-1]
